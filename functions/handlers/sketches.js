f3e8cd3dd3fcb66699f864ce3cd841ad88b05114
Rhys Baker <rhys.baker99@gmail.com>
2020-04-24 15:15:52 +0100

    fetch comments, add comments, cleaned code slightly

--- functions/handlers/sketches.js
+++ functions/handlers/sketches.js
@@ -8,7 +8,7 @@ exports.getAllSketches = (req, res) => {
             let sketches = [];
             data.forEach((doc) => {
                 sketches.push({
-                    sketchID: doc.id,
+                    sketchId: doc.id,
                     body: doc.data().body,
                     userHandle: doc.data().userHandle,
                     createdAt: doc.data().createdAt
@@ -40,3 +40,63 @@ exports.postSketch = (req, res) => {
             console.error(err);
         })
 }
+
+//fetch one sketch
+exports.getSketch = (req, res) => {
+    let sketchData = {};
+
+    db.doc(`/sketches/${req.params.sketchId}`).get()
+        .then(doc => {
+            if (!doc.exists) {
+                return res.status(400).json({ error: 'Sketch not found' })
+            }
+            sketchData = doc.data();
+            sketchData.sketchId = doc.id;
+            return db.collection('comments').orderBy('createdAt', 'desc').where('sketchId', '==', req.params.sketchId).get();
+        })
+        .then(data => {
+            sketchData.comments = [];
+            data.forEach(doc => {
+                sketchData.comments.push(doc.data())
+            });
+            return res.json(sketchData)
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: err.code })
+        })
+};
+
+// Comment on a sketch
+exports.commentOnSketch = (req, res) => {
+    if (req.body.body.trim() === '')
+        return res.status(400).json({ comment: 'Must not be empty' });
+
+    const newComment = {
+        body: req.body.body,
+        createdAt: new Date().toISOString(),
+        sketchId: req.params.sketchId,
+        userHandle: req.user.handle,
+        imageUrl: req.user.imageUrl
+    };
+    console.log(newComment);
+
+    db.doc(`/sketches/${req.params.sketchId}`)
+        .get()
+        .then((doc) => {
+            if (!doc.exists) {
+                return res.status(404).json({ error: 'sketch not found' });
+            }
+            return doc.ref.update({ commentCount: doc.data().commentCount + 1 });
+        })
+        .then(() => {
+            return db.collection('comments').add(newComment);
+        })
+        .then(() => {
+            res.json(newComment);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ error: 'Something went wrong' });
+        });
+};
--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,17 @@ const app = require('express')();
 
 const FBAuth = require('./util/FBAuth');
 
-const { getAllSketches, postSketch } = require('./handlers/sketches');
+const { getAllSketches, postSketch, getSketch, commentOnSketch } = require('./handlers/sketches');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/user');
 
 //sketch route
 app.get('/sketches', getAllSketches);
 app.post('/sketch', FBAuth, postSketch);
+app.get('/sketch/:sketchId', getSketch);
+//TODO: Delete sketch
+// TODO: Like a sketch
+// TODO: unlike sketch
+app.post('/sketch/:sketchId/comment', FBAuth, commentOnSketch)
 
 //user routes
 app.post('/signup', signup);
@@ -17,6 +22,5 @@ app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser)
-
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
